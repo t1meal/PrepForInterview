797cc01b302818a2f21224df076a3a80557b16f8
Алексей <t1meal@yandex.ru>
2023-01-30 19:54:35 +0300

    hm_3. some fix

--- src/main/java/ru/gb/lesson_3/counter/Test.java
+++ src/main/java/ru/gb/lesson_3/counter/Test.java
@@ -6,7 +6,7 @@ public class Test {
         SyncCounter counter = new SyncCounter();
         new Thread(counter::inc).start();
         new Thread(counter::dec).start();
-        new Thread(() -> System.out.println(counter.get())).start();
         new Thread(counter::inc).start();
+        new Thread(counter::dec).start();
     }
 }
