2690697e501eb7bd56f950b5371f04829676bdaa
Алексей <t1meal@yandex.ru>
2023-02-13 19:53:31 +0300

    hm_7.

--- students_module/src/main/java/ru/gb/students_module/MainApplication.java
+++ students_module/src/main/java/ru/gb/students_module/MainApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lesson7Application {
+public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Lesson7Application.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
 }
